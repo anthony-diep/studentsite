a77e33a066d094b045950e879722631591c7da98
NMA Student <nmastudent@KOAMAC015.local>
2020-03-10 10:35:47 -1000

    FInished togglenav

--- art128/base-site-togglenav/js/script.js
+++ art128/base-site-togglenav/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // TOGGLE NAV
     var mymenubutton = document.querySelector('.menu-button');
-    var mytogglemenu = document.querySelector('.toggle.nav');
+    var mytogglemenu = document.querySelector('.toggle-nav');
 
     mymenubutton.addEventListener('click', function () {
         mytogglemenu.classList.toggle('expanded');
